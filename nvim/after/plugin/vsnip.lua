f0f2c6ae32e6d8ac858937dc973372340a793e63
Zachary Ho <zachyho@canva.com>
2022-06-18 13:15:42 +1000

    updated zsh aliases. fixed vsnip keymaps

--- nvim/after/plugin/vsnip.lua
+++ nvim/after/plugin/vsnip.lua
@@ -1,15 +1,26 @@
 -- Expand
-map({'i', 's'}, '<expr> <C-j>', "vsnip#expandable() ? '<Plug>(vsnip-expand)' : '<C-j>'", { noremap = false })
+map({ "i", "s" }, "<C-j>", "vsnip#expandable() ? '<Plug>(vsnip-expand)' : '<C-j>'", { noremap = false, expr = true })
 
 -- Expand or jump
-map({'i', 's'}, '<expr> <C-l>', "vsnip#available(1) ? '<Plug>(vsnip-expand-or-jump)' : '<C-l>'", { noremap = false })
+map(
+	{ "i", "s" },
+	"<C-l>",
+	"vsnip#available(1) ? '<Plug>(vsnip-expand-or-jump)' : '<C-l>'",
+	{ noremap = false, expr = true }
+)
 
 -- Jump forward or backward
-map({'i', 's'}, '<expr> <Tab>', "vsnip#jumpable(1) ? '<Plug>(vsnip-jump-next)' : '<Tab>'", { noremap = false })
-map({'i', 's'}, '<expr> <S-Tab>', "vsnip#jumpable(-1) ? '<Plug>(vsnip-jump-prev)' : '<S-Tab>'", { noremap = false })
+map({ "i", "s" }, "<Tab>", "vsnip#jumpable(1) ? '<Plug>(vsnip-jump-next)' : '<Tab>'", { noremap = false, expr = true })
+map(
+	{ "i", "s" },
+	"<S-Tab>",
+	"vsnip#jumpable(-1) ? '<Plug>(vsnip-jump-prev)' : '<S-Tab>'",
+	{ noremap = false, expr = true }
+)
 
 -- E.g. vsnip_filetypes.typescript = ['javascript'] means we can use js snippets in .ts files
 vim.cmd([[
+    let g:vsnip_snippet_dir = expand('$HOME/.config/dotfiles/nvim/vsnip/.vsnip')
     let g:vsnip_filetypes = {}
     let g:vsnip_filetypes.javascriptreact = ['javascript']
     let g:vsnip_filetypes.typescriptreact = ['typescript', 'javascript']
--- nvim/lua/zachyho/lsp/null-ls.lua
+++ nvim/lua/zachyho/lsp/null-ls.lua
@@ -27,9 +27,9 @@ M.setup = function()
 			diagnostics.eslint_d.with({
 				prefer_local = "node_modules/.bin",
 			}),
-			-- formatting.prettier.with({
-			-- only_local = "node_modules/.bin",
-			-- }),
+			formatting.prettier.with({
+				only_local = "node_modules/.bin",
+			}),
 			formatting.stylua,
 		},
 	})
