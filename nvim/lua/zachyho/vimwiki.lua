8a993a9974932a203c3fb64e180193b1caed97cd
Zachary Ho <zachyho@canva.com>
2022-07-09 13:44:14 +1000

    removed old vimwiki stuff

--- nvim/lua/zachyho/vimwiki.lua
+++ nvim/lua/zachyho/vimwiki.lua
@@ -4,19 +4,10 @@ local baseWiki = {
 	syntax = "markdown",
 	ext = ".md",
 }
-local notesWiki = {
-	path = "~/zachyho-gits/vimwiki/notes",
-	syntax = "markdown",
-	ext = ".md",
-	auto_tags = 1,
-	auto_generate_links = 1,
-}
 
-vim.g.vimwiki_list = { baseWiki, notesWiki }
+vim.g.vimwiki_list = { baseWiki }
 
 vim.g.vimwiki_folding = "expr"
 
 map("n", "<leader>wh", "<Plug>Vimwiki2HTML")
 map("n", "<leader>whh", "<Plug>Vimwiki2HTMLBrowse")
-map("n", "<leader>vgt", ":VimwikiGenerateTagLinks<CR>")
-map("n", "<leader>vst", ":VimwikiSearchTags ")
