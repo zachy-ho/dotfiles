ed6351289106813f742da8e12bd2c40d62c506cb
Zachary Ho <zyyho97@gmail.com>
2022-10-03 21:14:54 +1100

    sync

--- nvim/lua/zachyho/plugins/vimwiki/boilerplate.lua
+++ nvim/lua/zachyho/plugins/vimwiki/boilerplate.lua
@@ -1,14 +1,23 @@
 vim.cmd([[
     function VimwikiDiaryBoilerplate()
+        " 'normalized' means that the strings are single-digit if the dates are single-digit
+        let filename_day = split(split(expand('%:r'),'/')[-1], '-')[-1]
+        let filename_day_normalized = string(str2nr(filename_day))
+        let today_day = string(str2nr(strftime("%d")))
+        let today_day_normalized = string(str2nr(strftime("%d")))
+        let tomorrow_day_normalized = string(str2nr(strftime('%d')) + 1)
+
         " File is today's diary
-        if split(split(expand('%:r'),'/')[-1], '-')[-1] == strftime("%d")
+        if filename_day_normalized == today_day_normalized
             let date = "# " . strftime("%A, %d %B %G")
-        elseif split(split(expand('%:r'),'/')[-1], '-')[-1] == string(str2nr(strftime('%d')) + 1)
+        elseif filename_day_normalized == tomorrow_day_normalized
         " File is tomorrow's diary
             let date = "# " . strftime("%A, %d %B %G", localtime() + (24 * 3600))
+        else
+            echo "Vimwiki boilerplating didn't happen for some reason"
         endif
         " Ignore all other days
-        call append(0, [date, '', '## todo', '- [ ] my',  '- [ ] tings',   '- [ ] today', '', '## growth', 'Today, I...' ])
+        call append(0, [date, '', '## todo', '- [ ] my', '- [ ] tings', '- [ ] today', '', '## growth', 'Today, I...' ])
     endfunction
 
     au BufNewFile ~/vimwiki/diary/*.md call VimwikiDiaryBoilerplate()
