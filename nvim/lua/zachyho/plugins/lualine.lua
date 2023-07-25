b21e0c7683ef151386441b009f5e2f8ba417b3aa
zachy-ho <zachyho@canva.com>
2023-07-25 17:39:44 +1000

    sync

--- nvim/lua/zachyho/plugins/lualine.lua
+++ nvim/lua/zachyho/plugins/lualine.lua
@@ -11,6 +11,14 @@ return {
 			options = {
 				theme = "gruvbox-material",
 			},
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "branch", "diff", "diagnostics" },
+				lualine_c = { "filename" },
+				lualine_x = { "filetype" },
+				lualine_y = { "progress" },
+				lualine_z = { "location" },
+			},
 		})
 	end,
 }
