46a5d3d368ff5f18a5eb42e7be0a2d85a512bfd9
zachy-ho <zachyho@canva.com>
2024-01-17 19:25:35 +0800

    simplified some nested modules

--- nvim/lua/zachyho/plugins/init.lua
+++ nvim/lua/zachyho/plugins/init.lua
@@ -1,91 +1,87 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 local prepare_plugin_specs = function()
-	---@type table List of Lua module names in the plugin directory, to be set up in order.
-	local plugins = {
-		"plenary",
-		"colorscheme",
-		"nvim_web_devicons",
-		"lualine",
-		"autopairs",
-		"smartcolumn",
-		"nvim_spider",
-		"nerdcommenter",
-		"markdown_preview",
-		"prettier",
-		"emmet",
-		"easy_align",
-		"nvim_surround",
-		"fugitive",
-		"git_conflict",
-		"fzf_lua",
-		----- These MUST come before nvim-lspconfig! -----
-		"mason",
-		"mason_lspconfig",
-		"neodev",
-		"typescript_tools",
-		"cmp_nvim_lsp",
-		"lspkind_nvim",
-		"nvim_lspconfig.null_ls",
-		"nvim_lspconfig",
+    ---@type table List of Lua module names in the plugin directory, to be set up in order.
+    local plugins = {
+        "plenary",
+        "colorscheme",
+        "nvim_web_devicons",
+        "lualine",
+        "autopairs",
+        "smartcolumn",
+        "nvim_spider",
+        "nerdcommenter",
+        "markdown_preview",
+        "prettier",
+        "emmet",
+        "easy_align",
+        "nvim_surround",
+        "fugitive",
+        "git_conflict",
+        "fzf_lua",
+        ----- These MUST come before nvim-lspconfig! -----
+        "mason",
+        "mason_lspconfig",
+        "neodev",
+        "typescript_tools",
+        "cmp_nvim_lsp",
+        "lspkind_nvim",
+        "nvim_lspconfig",
 
-		"telescope",
-		"telescope_fzf_native",
-		"harpoon",
-		"vsnip.vim_vsnip",
-		"vsnip.cmp_vsnip",
-		"vsnip.vim_vsnip_integ",
-		"nvim_cmp",
-		"nvim_cmp.sources",
-		"nvim_tree",
-		"nvim_treesitter",
-		"nvim_treesitter_context",
-		"vim_kitty",
-	}
+        "telescope",
+        "telescope_fzf_native",
+        "harpoon",
+        "vsnip",
+        "nvim_cmp",
+        "nvim_tree",
+        "nvim_treesitter",
+        "nvim_treesitter_context",
+        "vim_kitty",
+    }
 
-	local spec_initialisers = {}
-	for i, filename in ipairs(plugins) do
-		local full_filename = "zachyho.plugins." .. filename
-		spec_initialisers[i] = function()
-			local res = safe_require(full_filename)
-			return res or nil
-		end
-	end
+    local spec_initialisers = {}
+    for i, filename in ipairs(plugins) do
+        local full_filename = "zachyho.plugins." .. filename
+        spec_initialisers[i] = function()
+            local res = safe_require(full_filename)
+            return res or nil
+        end
+    end
 
-	local specs = {}
-	for _, init in ipairs(spec_initialisers) do
-		local spec = init()
-		if spec then
-			table.insert(specs, spec)
-		end
-	end
-	table.insert(
-		specs,
-		-- my local complex.nvim plugin
-		{
-			dir = "~/projects/complex.nvim",
-			dependencies = { "nvim-treesitter/nvim-treesitter" },
-			config = function()
-				require("complex")
-				-- dev
-				map("n", "<leader>dl", ":lua package.loaded['complex'] = nil<CR>")
-				map("n", "<leader>cp", ":lua require'complex'.get_function_complexity()<CR>")
-				map("n", "<leader>rt", ":PlenaryBustedFile %<CR>")
-			end,
-		}
-	)
-	return specs
+    local specs = {}
+    for _, init in ipairs(spec_initialisers) do
+        local spec = init()
+        if spec then
+            table.insert(specs, spec)
+        end
+    end
+    table.insert(
+        specs,
+        -- my local complex.nvim plugin
+        {
+            dir = "~/projects/complex.nvim",
+            dependencies = { "nvim-treesitter/nvim-treesitter" },
+            config = function()
+                require("complex")
+                -- dev
+                map("n", "<leader>dl", ":lua package.loaded['complex'] = nil<CR>")
+                map("n", "<leader>cp", ":lua require'complex'.get_function_complexity()<CR>")
+                map("n", "<leader>rt", ":PlenaryBustedFile %<CR>")
+            end,
+        }
+    )
+    return specs
 end
 
 local specs = prepare_plugin_specs()
---
+++ nvim/lua/zachyho/plugins/nvim_cmp.lua
@@ -0,0 +1,83 @@
+return {
+    {
+        "hrsh7th/nvim-cmp",
+        dependencies = {
+            "nvim-lspconfig",
+            "cmp-vsnip",
+            "vim-vsnip",
+            "vim-vsnip-integ",
+        },
+        config = function()
+            -- E.g. vsnip_filetypes.typescript = ['javascript'] means we can use js snippets in .ts files
+            vim.cmd([[
+            let g:vsnip_snippet_dir = expand('$HOME/.config/dotfiles/nvim/vsnip/.vsnip')
+            let g:vsnip_filetypes = {}
+            let g:vsnip_filetypes.javascriptreact = ['javascript']
+            let g:vsnip_filetypes.typescriptreact = ['typescript', 'javascript']
+            let g:vsnip_filetypes.typescript = ['javascript']
+        ]])
+            local cmp = require("cmp")
+            local lspkind = require("lspkind")
+
+            vim.o.completeopt = "menu,menuone,noselect"
+
+            cmp.setup({
+                -- NOTE: A snippet engine MUST be specified (currently vsnip)
+                snippet = {
+                    expand = function(args)
+                        vim.fn["vsnip#anonymous"](args.body)
+                    end,
+                },
+                mapping = cmp.mapping.preset.insert({
+                    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+                    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+                    ["<C-Space>"] = cmp.mapping.complete(),
+                    ["<C-e>"] = cmp.mapping.close(),
+                    ["<CR>"] = cmp.mapping.confirm({ select = false }),
+                }),
+                sources = cmp.config.sources({
+                    -- Neovim builtin LSP client
+                    {
+                        name = "nvim_lsp",
+                        entry_filter = function(entry)
+                            -- Fuck snippets. They pollute my autocompletions
+                            -- See :h nvim-cmp 'entry_filter'
+                            return cmp.lsp.CompletionItemKind[entry:get_kind()] ~= "Snippet"
+                        end,
+                    },
+                    -- Vsnip
+                    -- { name = "vsnip" },
+                    -- Neovim Lua API
+                    { name = "nvim_lua" },
+                    -- Filesystem paths
+                    { name = "path" },
+                    -- Buffer words
+                    { name = "buffer" },
+                }),
+                formatting = {
+                    format = lspkind.cmp_format({ with_text = true, maxwidth = 50 }),
+                },
+                window = {
+                    completion = cmp.config.window.bordered(),
+                    documentation = cmp.config.window.bordered(),
+                },
+            })
+        end,
+    },
+    -- Cmp source for buffer
+    {
+        "hrsh7th/cmp-buffer",
+        dependencies = "nvim-cmp",
+    },
+    -- Cmp source for path
+    {
+
+        "hrsh7th/cmp-path",
+        dependencies = "nvim-cmp",
+    },
+    -- Cmp source for lua
+    {
+        "hrsh7th/cmp-nvim-lua",
+        dependencies = "nvim-cmp",
+    },
+}
