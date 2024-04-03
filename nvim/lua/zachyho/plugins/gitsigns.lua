39c723480b9488af15c6bf0da44c8552cd8b5b74
zachy-ho <zachyho@canva.com>
2024-04-03 23:43:19 +1100

    made gitsigns config better

--- nvim/lua/zachyho/plugins/gitsigns.lua
+++ nvim/lua/zachyho/plugins/gitsigns.lua
@@ -7,10 +7,16 @@ return {
 			linehl = true,
 			word_diff = true,
 		})
-		map("n", "<leader>gs", ":Gitsigns ")
+		map("n", "<leader>gsl", function()
+			require("gitsigns").toggle_deleted(false)
+			require("gitsigns").toggle_word_diff()
+			require("gitsigns").toggle_linehl()
+			require("gitsigns").toggle_numhl()
+			require("gitsigns").toggle_signs()
+		end)
 	end,
 	lazy = true,
 	keys = {
-		{ "<leader>gs", ":Gitsigns " },
+		{ "<leader>gsp", ":Gitsigns ", "gitsigns pick" },
 	},
 }
