00d27825cc0e9c8f00679a76eb53ec7b536f85d3
zachy-ho <zachyho@canva.com>
2024-06-03 09:26:22 +1000

    simplify mason lsp config setup

--- nvim/lua/zachyho/plugins/mason_lspconfig.lua
+++ nvim/lua/zachyho/plugins/mason_lspconfig.lua
@@ -2,16 +2,6 @@ return {
 	"williamboman/mason-lspconfig.nvim",
 	dependencies = "williamboman/mason.nvim",
 	config = function()
-		-- Mason lsp
-
-		-- TODO handle new server configs
-		-- local server_configs = require("zachyho.plugins.nvim_lspconfig.server_configs")
-		-- local ensure_installed = {}
-		-- if server_configs then
-		-- ensure_installed = table_utils.get_keys(server_configs)
-		-- end
-		require("mason-lspconfig").setup({
-			-- ensure_installed,
-		})
+		require("mason-lspconfig").setup()
 	end,
 }
